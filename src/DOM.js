4627644c0e6da4fc1f04e4d4312bea717c884e84
Сажинов игорь <ia.sazhinov@tensor.ru>
2022-03-23 18:52:01 +0300

    1 коммит

--- src/DOM.js
+++ src/DOM.js
@@ -4,8 +4,14 @@
   Необходимо, чтобы функция осуществила вставку на страницу указанный тег с указанным содержимым указанное число раз.
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
-export function appendToBody(tag, content, count) {
-}
+// export function appendToBody(tag, content, count) {
+//   for (let i = 0; i < count; i++) {
+//     let dom = document.querySelector('body');
+//     let newBox = document.createElement(`${tag}`);
+//     newBox.innerHTML = `${content}`;
+//     dom.appendChild(newBox);
+//   }
+// }
 
 /*
   Создайте дерево вложенных тегов DIV.
@@ -14,9 +20,21 @@ export function appendToBody(tag, content, count) {
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
-export function generateTree(childrenCount, level) {
+/* export */ function generateTree(childrenCount, level) {
+    let dom = document.querySelector('body');
+    for (let i = 0; i < childrenCount; i++) {
+        let count = 0;
+        dom = document.querySelector('body');
+        for (let n = 0; n < level; n++) {
+            let newBox = document.createElement('div');
+            newBox.className = `item_${count + 1}`;
+            dom.appendChild(newBox);
+            dom = document.querySelector(`.item_${count + 1}`);
+            count++;
+        }
+    }
 }
-
+generateTree(3, 2);
 /*
   Используйте функцию для создания дерева тегов DIV из предыдущего задания.
   Создайте дерево с вложенностью 3 и числом элементов в каждом узле 2.
@@ -25,5 +43,5 @@ export function generateTree(childrenCount, level) {
   которые находились внутри переписанных тегов.
   Сформированное дерево верните в качестве результата работы функции.
 */
-export function replaceNodes() {
-}
+// export function replaceNodes() {
+// }
