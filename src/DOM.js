41b7b6f46c52bdf521f826d93e07b76413f03013
Сажинов игорь <ia.sazhinov@tensor.ru>
2022-06-14 17:25:55 +0300

    tast DOM

--- src/DOM.js
+++ src/DOM.js
@@ -4,14 +4,14 @@
   Необходимо, чтобы функция осуществила вставку на страницу указанный тег с указанным содержимым указанное число раз.
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
-// export function appendToBody(tag, content, count) {
-//   for (let i = 0; i < count; i++) {
-//     let dom = document.querySelector('body');
-//     let newBox = document.createElement(`${tag}`);
-//     newBox.innerHTML = `${content}`;
-//     dom.appendChild(newBox);
-//   }
-// }
+export function appendToBody(tag, content, count) {
+    for (let i = 0; i < count; i++) {
+        let dom = document.querySelector('body');
+        let newBox = document.createElement(`${tag}`);
+        newBox.innerHTML = `${content}`;
+        dom.appendChild(newBox);
+    }
+}
 
 /*
   Создайте дерево вложенных тегов DIV.
@@ -20,21 +20,24 @@
   Каждый элемент должен иметь класс вида item_n, где n - глубина вложенности элемента. (Нумерацию ведем с единицы).
   Сформированное дерево верните в качестве результата работы функции.
 */
-/* export */ function generateTree(childrenCount, level) {
-    let dom = document.querySelector('body');
-    for (let i = 0; i < childrenCount; i++) {
-        let count = 0;
-        dom = document.querySelector('body');
-        for (let n = 0; n < level; n++) {
-            let newBox = document.createElement('div');
-            newBox.className = `item_${count + 1}`;
-            dom.appendChild(newBox);
-            dom = document.querySelector(`.item_${count + 1}`);
-            count++;
+export function generateTree(childrenCount, level) {
+    let div = document.createElement('div');
+    div.classList.add('item_1');
+    document.body.append(div);
+
+    for (let i = 1; i < level; i++) {
+        let current_lvl = document.getElementsByClassName(`item_${i}`);
+        for (let k of current_lvl) {
+            for (let j = 0; j < childrenCount; j++) {
+                const newDiv = document.createElement('div');
+                newDiv.classList.add(`item_${i + 1}`);
+                k.append(newDiv);
+            }
         }
     }
+    return div;
 }
-generateTree(3, 2);
+
 /*
   Используйте функцию для создания дерева тегов DIV из предыдущего задания.
   Создайте дерево с вложенностью 3 и числом элементов в каждом узле 2.
@@ -43,5 +46,21 @@ generateTree(3, 2);
   которые находились внутри переписанных тегов.
   Сформированное дерево верните в качестве результата работы функции.
 */
-// export function replaceNodes() {
-// }
+export function replaceNodes() {
+    generateTree(2, 3);
+    let item_2 = document.querySelector('.item_2');
+    console.log(item_2);
+    for (let i = 0; i < 2; i++) {
+        let item = document.querySelector('div.item_2');
+        let newTag = document.createElement('SECTION');
+        newTag.classList.add('item_2');
+        item.before(newTag);
+        let childNodes = item.childNodes;
+        for (true; childNodes.length > 0; true) {
+            newTag.prepend(childNodes[0]);
+        }
+        item.parentElement.removeChild(item);
+    }
+    let item_1 = document.querySelector('.item_1');
+    return item_1;
+}
